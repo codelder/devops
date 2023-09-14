8867c524f918105d2d5dfb703ed103bbdfacd66b
codelder <codelder@ibankapp.org>
2023-09-14 17:31:53 +0800

    add AccountService

---
+++ classic/src/main/java/com/example/classic/services/AccountService.java
@@ -0,0 +1,10 @@
+package com.example.classic.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountService {
+    public String account(){
+        return "I'm account";
+    }
+}
--- classic/src/main/java/com/example/classic/services/SettlementService.java
+++ classic/src/main/java/com/example/classic/services/SettlementService.java
@@ -1,10 +1,19 @@
 package com.example.classic.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class SettlementService {
+
+    private AccountService accountService;
+
+    @Autowired
+    public void setAccountService(AccountService accountService){
+        this.accountService = accountService;
+    }
     public String settle(){
-        return "i'm settle";
+        return accountService.account()+ "<br/>i'm settle";
     }
 }
