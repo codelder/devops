e90c95b597f3f89f95378bcd3bed306b925ceb00
codelder <codelder@ibankapp.org>
2023-09-14 16:45:04 +0800

    remove unused parameter

--- classic/src/main/java/com/example/classic/ClassicApplication.java
+++ classic/src/main/java/com/example/classic/ClassicApplication.java
@@ -5,22 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ClassicApplication {
 
-	@Autowired
 	private SettlementService settlementService;
+
+	@Autowired
+	public void setSettlementService(SettlementService settlementService){
+		this.settlementService = settlementService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassicApplication.class, args);
 	}
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String greeting() {
 		return String.format(settlementService.settle());
 	}
 
