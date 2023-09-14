1c1cc126b0025ba21dd91fd7e82744863aedad90
codelder <codelder@ibankapp.org>
2023-09-14 16:36:49 +0800

    add controller

--- classic/src/main/java/com/example/classic/ClassicApplication.java
+++ classic/src/main/java/com/example/classic/ClassicApplication.java
@@ -1,13 +1,27 @@
 package com.example.classic;
 
+import com.example.classic.services.SettlementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClassicApplication {
+
+	@Autowired
+	private SettlementService settlementService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassicApplication.class, args);
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format(settlementService.settle());
 	}
 
 }
---
+++ classic/src/main/java/com/example/classic/services/SettlementService.java
@@ -0,0 +1,10 @@
+package com.example.classic.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SettlementService {
+    public String settle(){
+        return "i'm settle";
+    }
+}
