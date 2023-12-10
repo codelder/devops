905d38497b119bbde9e0730b61361053793c9086
codelder <codelder@ibankapp.org>
2023-12-10 21:13:05 +0800

    completed register

---
+++ register/src/main/java/com/example/register/config/AccountConfig.java
@@ -0,0 +1,9 @@
+package com.example.register.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-account.yaml")
+public class AccountConfig {
+}
